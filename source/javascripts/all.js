c07b6f7cbd3c243ca602b388abd10c8c4dede6d5
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-07-26 17:04:34 +0200

    reworked navbar and behavior

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,3 +2,19 @@
 //= require materialize
 //= require jquery-validation
 //= require_tree .
+
+$(document).ready(function() {
+  function scroll_to_anchor(anchor_id){
+    var tag = $(anchor_id);
+    $('html, body').scrollTop(tag.offset().top - 64);
+  };
+
+  $(".nav-tab").click(function(e){
+    e.preventDefault();
+    $('.nav-tab').removeClass('active');
+    $(this).addClass('active');
+    $('.tab-content').removeClass('active-content');
+    $($(this).attr('href')).addClass('active-content');
+    scroll_to_anchor($(this).attr('href'));
+  });
+});
